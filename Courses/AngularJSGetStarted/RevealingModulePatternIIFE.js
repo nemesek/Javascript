f8b78ec9d6d3b63210e0ec4759d1922ece963ea5
Nemesek <dnemesek@gmail.com>
2014-09-02 22:01:21 -0500

    AngularJS course module 1

---
+++ Courses/AngularJSGetStarted/RevealingModulePatternIIFE.js
@@ -0,0 +1,29 @@
+// Code goes here
+
+(function() {
+  var createWorker = function() {
+
+    var workCount = 0;
+
+    var task1 = function() {
+      workCount += 1;
+      console.log("task one " + workCount);
+    };
+
+    var task2 = function() {
+      workCount += 1;
+      console.log("task two " + workCount);
+    };
+
+    return {
+      job1: task1,
+      job2: task2
+    };
+  };
+
+  var worker = createWorker();
+  worker.job1();
+  worker.job2();
+  worker.job1();
+})();
+
