908c416e0ab8c6124b785c164b7c021ccaebd909
Nemesek <dnemesek@gmail.com>
2014-09-03 21:02:50 -0500

    finished module 3 of course

---
+++ Courses/AngularJSGetStarted/module3/script.js
@@ -0,0 +1,34 @@
+// Code goes here
+
+(function() {
+  var app = angular.module("githubViewer", []);
+
+  var MainController = function($scope, $http) {
+
+    var onUserComplete = function(response) {
+      $scope.user = response.data;
+      $http.get($scope.user.repos_url)
+        .then(onRepos, onError);
+    };
+    
+    var onRepos = function(response){
+      $scope.repos = response.data;
+    };
+
+    var onError = function(reason) {
+      $scope.error = "Could not fetch the user";
+    };
+    
+    $scope.search = function(username){
+          $http.get("https://api.github.com/users/" + username)
+      .then(onUserComplete, onError);
+    };
+
+
+    
+    $scope.username = "Angular";
+    $scope.message = "Git Hub Viewer";
+    $scope.repoSortOrder = '-stargazers_count';
+  };
+  app.controller("MainController", ["$scope", "$http",MainController]);
+})();
