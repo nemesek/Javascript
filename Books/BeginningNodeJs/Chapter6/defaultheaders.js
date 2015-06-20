80dddebc2a520d3b5d7705b8198d0c524e7eb4ba
Nemesek <dnemesek@gmail.com>
2015-06-19 22:39:50 -0500

    more of chapter 6

---
+++ Books/BeginningNodeJs/Chapter6/defaultheaders.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+    console.log('request headers...');
+    console.log(req.headers);
+
+    // respond
+    res.write('hello dude');
+    res.end();
+
+}).listen(3000);
+
+console.log('server running on port 3000');
