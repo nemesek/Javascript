f5d0f07a305c9926a6195f1a45bc3e3ebeb4c998
Dan Nemesek <dnemesek@fncinc.com>
2015-11-13 20:57:49 -0600

    finished sorting of the table

--- Books/ReactUpAndRunning/Excel.js
+++ Books/ReactUpAndRunning/Excel.js
@@ -5,17 +5,41 @@ var Excel = React.createClass({
   },
   displayName : 'Excel',
   getInitialState: function() {
-    return {data: this.props.intialData};
+    return {
+      data : this.props.intialData,
+      sortby : null,
+      descending : false
+    };
   },
 
+  _sort : function (e) {
+
+    var column = e.target.cellIndex;
+    var descending = this.state.sortby === column && !this.state.descending;
+    // copy the data
+    var data = this.state.data.slice();
+
+    data.sort(function (a,b) {
+      return descending ? a[column] < b[column] : a[column] > b[column];
+    });
+
+    this.setState({
+      data : data,
+      sortby : column,
+      descending : descending
+    });
+  },
   render: function() {
   return (
     React.DOM.table(null,
-      React.DOM.thead(null,
+      React.DOM.thead({onClick: this._sort},
         React.DOM.tr(null,
           this.props.headers.map(function(title, idx) {
+            if (this.state.sortby === idx) {
+              title += this.state.descending ? '\u2191' : '\u2193'
+            }
             return React.DOM.th({key: idx}, title);
-          })
+          }.bind(this))
         )
       ),
       React.DOM.tbody(null,
